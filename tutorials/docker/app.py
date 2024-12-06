0e9fb114c8317d73ecdc9cbc1b5e3f5317c98760
The-CrazyMouse <andrerddantas@hotmail.com>
2024-12-06 17:46:02 +0000

    start docker tutorial

---
+++ tutorials/docker/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_worl():
+    return 'hello, World!'
+
+@app.route('/db')
+def db_connect():
+    connection = mysql.connector.connect(
+        host='db'
+        user='root'
+        password='root_password'
+        database='meu_database'
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT 'Conexão à base de dados bem-sucedida!'")
+    result = cursor.fetchone()
+    return result[0]
+
+if __name__ == '__name__':
+    app.run(host='0.0.0.0')
